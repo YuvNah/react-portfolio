c7032f86b6ecdcd167ac40f1e651df81cb107550
Yuval Nahmias <yuvalit789@gmail.com>
2024-03-26 13:38:26 -0400

    portfolio link almost works

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -7,8 +7,14 @@ export default function Portfolio() {
       <Header></Header>
       <h1>Portfolio</h1>
       <h2>Projects</h2>
-      <ul>
-        <li></li>
+      <ul className="project-group">
+        {/* Here we map over each grocery item and return a new array of `li` elements that contains the grocery name */}
+        {/* When using map you must provide a unique key attribute to each item. Ours is `item.id` */}
+        {portfolioData.map((project) => (
+          <li className="list-group-item" key={project.id}>
+            <a href={project.gitRepo}>{project.title}</a>
+          </li>
+        ))}
       </ul>
     </div>
   );
--- src/components/pages/data.js
+++ src/components/pages/data.js
@@ -1,13 +1,16 @@
 export const portfolioData = [
   {
+    id: 1,
     title: "Text Editor",
     gitrepo: "https://github.com/YuvNah/TextEditor",
   },
   {
+    id: 2,
     title: "Social Network API",
     gitrepo: "https://github.com/YuvNah/SocialNetworkAPI",
   },
   {
+    id: 3,
     title: "Recipe Sharing Portfolio",
     gitRepo: "https://github.com/Nikitaaa95/Project-2",
   },
